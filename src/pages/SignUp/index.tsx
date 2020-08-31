8d7e85b805db0bfcf4aab52f6acded89d1497848
Bruno Moura <bruno8moura@gmail.com>
2020-08-31 05:40:50 -0300

    SignUp screen.

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,56 @@
 import React from 'react';
-import { Container } from './styles';
+import {
+    Image,
+    KeyboardAvoidingView,
+    View,
+    ScrollView,
+    Platform,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import logoImg from '../../assets/logo.png';
 
-const SignUp: React.FC = () => <Container />;
+import { Container, Title, BackToSignIn, BackToSignInText } from './styles';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
+const SignUp: React.FC = () => (
+    <>
+        <KeyboardAvoidingView
+            style={{ flex: 1 }}
+            behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+            enabled
+        >
+            <ScrollView
+                contentContainerStyle={{ flex: 1 }}
+                keyboardShouldPersistTaps="handled"
+            >
+                <Container>
+                    <Image source={logoImg} />
+                    <View>
+                        <Title>Crie sua conta</Title>
+                    </View>
+                    <Input name="name" icon="user" placeholder="Nome" />
+                    <Input name="email" icon="mail" placeholder="E-mail" />
+                    <Input name="password" icon="lock" placeholder="Senha" />
+                    <Button
+                        onPress={() => {
+                            console.log('button pressed');
+                        }}
+                    >
+                        Cadastrar
+                    </Button>
+                </Container>
+            </ScrollView>
+        </KeyboardAvoidingView>
+
+        <BackToSignIn
+            onPress={() => {
+                console.log('Create account button pressed');
+            }}
+        >
+            <Icon name="arrow-left" size={20} color="#FFFFFF" />
+            <BackToSignInText>Voltar para logon</BackToSignInText>
+        </BackToSignIn>
+    </>
+);
 export default SignUp;
