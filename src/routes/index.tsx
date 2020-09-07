a2c872ad030c3feef97f762a96b963dd5756c88e
Bruno Moura <bruno8moura@gmail.com>
2020-09-07 07:17:40 -0300

    Unused import.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, ActivityIndicator, Text } from 'react-native';
+import { View, ActivityIndicator } from 'react-native';
 import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
 import { useAuth } from '../hooks/auth';
